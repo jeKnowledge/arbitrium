c26ff4f6c87cccfd1894a6d51d40fe2317250cbd
Teresa Margarida Baptista Cabral De Carvalho Salazar <tmsalazar@student.dei.uc.pt>
2017-09-29 19:11:34 +0100

    Fixes error.

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,3 +1,11 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+API_TOKEN = os.environ.get("API_TOKEN")
 DEFAULT_REPLY = "Sorry but I didn't understand you"
 PLUGINS = [
     'slackbot.plugins',
