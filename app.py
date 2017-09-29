eb0ceb4ce8ae74ebe92b4c6f05ea3e5366d2cb05
simaonogueira101 <simao.nogueira66@gmail.com>
2017-09-29 15:18:14 +0100

    Collects data from google sheet.

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from slackbot.bot import Bot
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import random
 
 def main():
+    scope = ['https://spreadsheets.google.com/feeds']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+    client = gspread.authorize(creds)
+    sheet = client.open('Arbitrium Test').sheet1
+
+    data = sheet.get_all_values()
+    index = 1
+    print(data[0])
+    print(data[1])
+    print(data[index][random.randint(0, len(data[index]) - 1)])
+
     bot = Bot()
     bot.run()
 
